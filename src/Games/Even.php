eb28cc4c5a2e25a061156de0428a6516c94e9400
Danila Zaitsev <shawn4easy@gmail.com>
2021-03-23 23:03:42 +0700

    Moved logic from bin to class. Added phpdoc

--- src/Games/Even.php
+++ src/Games/Even.php
@@ -10,13 +10,29 @@ class Even
     public const ANSWER_YES = 'yes';
     public const ANSWER_NO = 'no';
 
+    /**
+     * @return int
+     */
     public function generateRandNumber(): int
     {
         return rand(self::MIN_NUMBER, self::MAX_NUMBER);
     }
 
+    /**
+     * @param int $number
+     * @return bool
+     */
     public function isEven(int $number): bool
     {
         return $number % 2 === 0;
+    }
+
+    /**
+     * @param int $number
+     * @return string
+     */
+    public function getCorrectAnswer(int $number): string
+    {
+        return $this->isEven($number) ? self::ANSWER_YES : self::ANSWER_NO;
     }
 }
